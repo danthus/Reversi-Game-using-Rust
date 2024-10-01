154e19365a32279738119162f9cbc9be61dd97a5
Danthus <amoszhou8874@gmail.com>
2024-10-01 15:03:32 -0400

    version 2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::{io, simd::usizex1};
+use std::io;
+use std::process;
 
-static DECLARATION: &str = "ECE1724 Rust Assignment 1 Reversi Board Game by Amos Zhou";
-
+// initial board configutation
 fn init_board(board: &mut [[i32; 8]; 8]) {
     board[3][3] = 1;
     board[3][4] = 0;
@@ -9,8 +9,9 @@ fn init_board(board: &mut [[i32; 8]; 8]) {
     board[4][4] = 1;
 }
 
+// function to visualize board data
 fn print_board(board: &[[i32; 8]; 8]) {
-    println!("  a b c d e f g h");
+    println!("  abcdefgh");
 
     for (i, row) in board.iter().enumerate() {
         print!("{} ", (i+97) as u8 as char);
@@ -21,17 +22,19 @@ fn print_board(board: &[[i32; 8]; 8]) {
                 1 => "W",
                 _ => "?",
             };
-            print!("{} ", piece);
+            print!("{}", piece);
         }
         println!();
     }
 }
 
+// check the user input is valid to place
 fn check_placing(row: usize, col: usize, player: i32, board: &[[i32; 8]; 8]) -> bool {
     if board[row][col] != -1 {
         return false;
     }
 
+    // check the surronding 8 positions
     for irow in -1..=1 {
         for icol in -1..=1 {
             if irow==0 && icol==0{
@@ -46,6 +49,7 @@ fn check_placing(row: usize, col: usize, player: i32, board: &[[i32; 8]; 8]) -> 
     false
 }
 
+// check the given position has opponent piece and player piece exit on the continuous line
 fn check_line(row: isize, col: isize, irow: isize, icol: isize, player: i32, board: &[[i32; 8]; 8]) -> bool {
     let opponent = if player == 1 {0} else {1};
 
@@ -77,6 +81,7 @@ fn check_line(row: isize, col: isize, irow: isize, icol: isize, player: i32, boa
     false
 }
 
+// place the piece and reverse other pieces
 fn place_piece(row: usize, col: usize, player: i32, board: &mut [[i32; 8]; 8]) {
     board[row][col] = player;
 
@@ -93,25 +98,28 @@ fn place_piece(row: usize, col: usize, player: i32, board: &mut [[i32; 8]; 8]) {
 
 }
 
+// reverse other pieces
 fn reverse_pieces(row: isize, col: isize, irow: isize, icol: isize, player: i32, board: &mut [[i32; 8]; 8]) {
     let mut i: isize = row + irow;
     let mut j: isize = col + icol;
     
     while i>=0 && i<8 && j>=0 && j<8 {
-        if board[i][j] == player {
+        // reverse until a player's piece is met
+        if board[i as usize][j as usize] == player {
             break;
         }
 
-        board[i][j] = player;
+        board[i as usize][j as usize] = player;
 
         i += irow;
         j += icol;
     }
 }
 
+// check the given player has valid moves
 fn check_valid_moves(player: i32, board: &[[i32; 8]; 8]) -> bool {
-    for row in 0..=8 {
-        for col in 0..=8 {
+    for row in 0..8 {
+        for col in 0..8 {
             if check_placing(row, col, player, board) {
                 return true;
             }
@@ -120,30 +128,32 @@ fn check_valid_moves(player: i32, board: &[[i32; 8]; 8]) -> bool {
     false
 }
 
+// check winning condition and print
 fn check_win(board: &[[i32; 8]; 8]) {
     
     let (w_pieces,b_pieces) = count_pieces(board);
     if w_pieces > b_pieces {
-        println!("White wins by {} points!", {w_pieces - b_pieces});
+        print!("White wins by {} points!", {w_pieces - b_pieces});
     }
     else if b_pieces > w_pieces {
-        println!("Black wins by {} points!", {b_pieces - w_pieces});
+        print!("Black wins by {} points!", {b_pieces - w_pieces});
     }
     else {
-        println!("Draw!");
+        print!("Draw!");
     }
 }
 
+// count while and black pieces
 fn count_pieces(board: &[[i32; 8]; 8]) -> (isize, isize) {
     let mut w_pieces = 0;
     let mut b_pieces = 0;
 
-    for row in 0..=8 {
-        for col in 0..=8 {
+    for row in 0..8 {
+        for col in 0..8 {
             if board[row][col] == 1 {
                 w_pieces += 1;
             }
-            else if board[row][col] == -1 {
+            else if board[row][col] == 0 {
                 b_pieces += 1;
             }
         }
@@ -153,24 +163,29 @@ fn count_pieces(board: &[[i32; 8]; 8]) -> (isize, isize) {
 }
 
 fn main() {
+    let mut player_str: &str = "B"; // "B" for black, "W" for white, start with "B"
+    let mut player_int = 0; // 0 for black, 1 for white
 
-    // false is black, true is white
-    let mut player_str: &str = "B";
-    let mut player_int = 0;
-    
-    // -1 for empty, 1 for white, 0 for black
+    // -1 for empty, 1 for white, 0 for black, start with all -1
     let mut board: [[i32; 8]; 8] = [[-1; 8]; 8];
     init_board(&mut board);
     print_board(&board);
+    print!("Enter move for colour {} (RowCol): ", player_str);
 
+    // main game lopp starts
     loop {
-
-        println!("Enter move for colour {} (RowCol): ", player_str);
         let mut input = String::new();
 
         io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
+
+        let input = input.trim();
+
+        // special cmd to exit program
+        if input == "!q" {
+            process::exit(0);
+        }
 
         if input.len() != 2 {
             println!("Invalid input. Try again.");
@@ -182,11 +197,16 @@ fn main() {
 
         if row > 7 || col > 7 {
             println!("Invalid input. Try again.");
+            print_board(&board);
+            print!("Enter move for colour {} (RowCol): ", player_str);
             continue;
         }
 
+        // check the placing is valid or not
         if !check_placing(row, col, player_int, &board) { 
             println!("Invalid move. Try again.");
+            print_board(&board);
+            print!("Enter move for colour {} (RowCol): ", player_str);
             continue;
         }
 
@@ -196,19 +216,26 @@ fn main() {
         player_str = if player_str == "B" {"W"} else {"B"};
         player_int = if player_int == 0 {1} else {0};
 
+        print_board(&board);
 
+        // check if next player has valid moves or not
         if !check_valid_moves(player_int, &board) {
+            // if no valid move, switch back to previous player
             println!("{} player has no valid move.", player_str);
             player_str = if player_str == "B" {"W"} else {"B"};
             player_int = if player_int == 0 {1} else {0};
             
+            // check previous player has valid moves or not
             if !check_valid_moves(player_int, &board) {
+                // if both players have no valid moves, finish the game
                 println!("{} player has no valid move.", player_str);
                 check_win(&board);
+                process::exit(0);
             }
         }
 
-        break;
+        print!("Enter move for colour {} (RowCol): ", player_str);
+
     }
 
 }
